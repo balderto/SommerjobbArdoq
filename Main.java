4d20db1275d51e229d8373b421e07ff467c952d8
balderto <79977967+balderto@users.noreply.github.com>
2021-10-14 23:23:32 +0200

    spelling error
    last commit was for changing the comments

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.PriorityQueue;
 public class Main {
 	public static void main(String[] args) {
 		
-		//Here is an example of how to use my HighestProduct class to produce the highest factor, givwen a number of factor and a list.
+		//Here is an example of how to use my HighestProduct class to produce the highest factor, given a number of factor and a list.
 		final int NUMBER_OF_FACTORS = 3;
 		int[] list = new int[] {1, 10, 7, 6, 5, 3};
 		System.out.println(HighestProduct.getHighestProduct(NUMBER_OF_FACTORS, list));
