d7ae140841988a727a6e8a3707ba98e780b64510
balderto <79977967+balderto@users.noreply.github.com>
2021-10-14 23:22:54 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.PriorityQueue;
 public class Main {
 	public static void main(String[] args) {
 		
-		//The complexity in big O notation here would be O(n) which is optimal as we necessarily have to look at each number at least once to decide which ones are the largest.
+		//Here is an example of how to use my HighestProduct class to produce the highest factor, givwen a number of factor and a list.
 		final int NUMBER_OF_FACTORS = 3;
 		int[] list = new int[] {1, 10, 7, 6, 5, 3};
 		System.out.println(HighestProduct.getHighestProduct(NUMBER_OF_FACTORS, list));
